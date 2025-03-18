dc5653eaf2776dc0385975917ebf268f978c5d0d
MrMrProgrammer <mr.mr.programmer@gmail.com>
2025-03-18 11:48:45 +0330

    add first todo api with Serializer

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
 
-# Create your views here.
+from .serializers import Todoserializer
+from .models import Todo
+
+# ------------------------------------------------------------------------------------------------
+
+# Without Serializer
+# @api_view(['GET'])
+# def todos(request: Request):
+#     todos: list[Todo] = Todo.objects.order_by('priority').all().values('title', 'is_done')
+#     return Response({"todos": todos}, status.HTTP_200_OK)
+
+# With Serializer
+@api_view(['GET'])
+def todos(request: Request):
+    todos = Todo.objects.order_by('priority').all()
+    todo_serializer = Todoserializer(todos, many=True)
+    return Response(todo_serializer.data, status.HTTP_200_OK)
+
+# ------------------------------------------------------------------------------------------------
